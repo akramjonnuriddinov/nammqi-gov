f819969c4d177208f5b3fb79e87726b934f8e00e
akramjonnuriddinov <nn.akramjon@gmail.com>
2025-01-22 16:51:57 +0500

    add header

--- components/app/Footer.vue
+++ components/app/Footer.vue
@@ -1,3 +1,5 @@
 <script setup lang="ts"></script>
 
-<template>Footer</template>
+<template>
+  <footer>Footer</footer>
+</template>
--- components/app/Header.vue
+++ components/app/Header.vue
@@ -1,3 +1,28 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const links = [
+  {
+    name: "Home",
+    path: "/",
+  },
+  {
+    name: "About",
+    path: "/about",
+  },
+  {
+    name: "Blog",
+    path: "/blog",
+  },
+]
+</script>
 
-<template>Header</template>
+<template>
+  <header class="bg-gray-500 text-white">
+    <nav>
+      <ul class="flex gap-5">
+        <li v-for="(link, index) in links" :key="index">
+          <NuxtLink :to="link.path">{{ link.name }}</NuxtLink>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
