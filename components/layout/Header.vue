ae393a421f7537da9992a3de456cff7ca9dd3780
abdumajidovismoilbek007 <abdumajidovismoilbek007@gmail.com>
2025-01-27 10:47:48 +0500

    update all

--- components/layout/Header.vue
+++ components/layout/Header.vue
@@ -18,9 +18,9 @@ const links = [
 <template>
   <header class="h-[100px] p-7 flex items-center justify-between">
     <div class="font-bold">Institutning interaktiv <br> xizmatlar portali</div>
-    <div class="relative border border-main-500 rounded-md flex items-center px-[1px] w-[400px]">
+    <div class="relative border border-main-500 rounded-xl flex items-center px-[1px] w-[400px]">
       <input class="py-2 px-3 border-transparent w-full" type="text" placeholder="Xizmatni izlsh">
-      <button class="bg-main-500 fill-white rounded-md px-3 py-2">
+      <button class="bg-main-500 fill-white rounded-xl px-3 py-2">
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
           <path
             d="M10 18a7.952 7.952 0 0 0 4.897-1.688l4.396 4.396 1.414-1.414-4.396-4.396A7.952 7.952 0 0 0 18 10c0-4.411-3.589-8-8-8s-8 3.589-8 8 3.589 8 8 8zm0-14c3.309 0 6 2.691 6 6s-2.691 6-6 6-6-2.691-6-6 2.691-6 6-6z">
